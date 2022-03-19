e5b5f8f967ed726aaae15509f5ee666acf4548a1
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2022-03-19 18:32:35 +0530

    eslint config; schema & query add

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/schema");
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 
-const app = express();
+const app = express()
 
-const PORT = 4000;
+const PORT = 4000
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
+    graphiql: true
   })
-);
+)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
