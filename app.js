77fb1e44d29d4db1d89c5561f40e04e59c8bf7b8
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2022-03-19 17:32:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+const PORT = 4000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
